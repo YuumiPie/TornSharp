8a115badf11948778d0d173f0d41a37a58a128eb
YuumiPie <altchanjeremy08@gmail.com>
2024-05-29 17:05:33 -0700

    Fixed UnixDateTimeConverter
    replaced UnixDateTimeConverter to a working model

--- TornSharp/CustomJsonConverter/UnixDateTimeConverter.cs
+++ TornSharp/CustomJsonConverter/UnixDateTimeConverter.cs
@@ -1,35 +1,25 @@
-using System.Globalization;
+using System.Buffers;
+using System.Buffers.Text;
+using System.Diagnostics;
 using System.Text.Json;
 using System.Text.Json.Serialization;
-using System.Text.RegularExpressions;
 
 namespace TornSharp.CustomJsonConverter;
-
 public class UnixDateTimeConverter : JsonConverter<DateTime>
 {
-    static readonly DateTime s_epoch = new(1970, 1, 1, 0, 0, 0);
-    static readonly Regex s_regex = new(@"^/Date\(([+-]*\d+)\)/$", RegexOptions.CultureInvariant);
-
     public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
-        string formatted = reader.GetString()!;
-        Match match = s_regex.Match(formatted);
-
-        if (
-            !match.Success
-            || !long.TryParse(match.Groups[1].Value, System.Globalization.NumberStyles.Integer, CultureInfo.InvariantCulture, out long unixTime))
-        {
-            throw new JsonException();
-        }
-
-        return s_epoch.AddMilliseconds(unixTime);
+        return DateTime.UnixEpoch.AddMilliseconds(reader.GetInt64());
     }
 
     public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
     {
-        long unixTime = Convert.ToInt64((value - s_epoch).TotalMilliseconds);
+        // The "R" standard format will always be 29 bytes.
+        Span<byte> utf8Date = new byte[29];
 
-        string formatted = string.Create(CultureInfo.InvariantCulture, $"/Date({unixTime})/");
-        writer.WriteStringValue(formatted);
+        bool result = Utf8Formatter.TryFormat(value, utf8Date, out _, new StandardFormat('R'));
+        Debug.Assert(result);
+
+        writer.WriteStringValue(utf8Date);
     }
 }
